564391438eeae50aa58f8613d4713d80af905cb1
Yi-Jyun Pan <me@pan93.com>
2025-12-15 15:05:52 +0800

    feat: update favicon

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+    useHead({
+        title: 'Yi-Jyun Pan',
+        meta: [
+            {
+                name: 'description',
+                content: 'Yi-Jyun Pan\'s CV.',
+            }
+        ]
+    })
+</script>
 <template>
     <div>
         <PortfolioHeader />
