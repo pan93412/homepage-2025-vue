8b721da2d1ea93e42c807b2ab33b640193ce74c7
Yi-Jyun Pan <me@pan93.com>
2025-12-15 22:54:03 +0800

    chore: clean up dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxtjs/i18n'],
+  modules: ['@nuxt/eslint', '@nuxtjs/i18n'],
   future: {
     compatibilityVersion: 5
   },
