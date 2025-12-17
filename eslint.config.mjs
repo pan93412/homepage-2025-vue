c977e2ee89e40b32a6d324ac243b60798158e8ac
Yi-Jyun Pan <me@pan93.com>
2025-12-18 01:35:18 +0800

    feat: add ESLint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,19 @@
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
+import eslintPluginBetterTailwindcss from "eslint-plugin-better-tailwindcss";
 
-export default withNuxt(); // Your custom configs here
+export default withNuxt([
+    {
+        plugins: {
+            "better-tailwindcss": eslintPluginBetterTailwindcss
+        },
+        rules: {
+            ...eslintPluginBetterTailwindcss.configs["recommended-warn"].rules,
+        },
+        settings: {
+            "better-tailwindcss": {
+                entryPoint: "app/assets/css/main.css",
+            }
+        }
+    }
+]); // Your custom configs here
